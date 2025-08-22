8e595aa40c0ee101a42ae4d5d821bdc00498c7b5
khushi1673 <63011366+khushi1673@users.noreply.github.com>
2025-08-22 22:17:56 +0530

    Create BRONZE_table_script.sql

---
+++ scripts/BRONZE_table_script.sql
@@ -0,0 +1,48 @@
+create or replace table dwh.bronze.crm_cust_info(
+cst_id int ,
+cst_key varchar(30),
+cst_firstname varchar(30),
+cst_lastname varchar(30),
+cst_marital_status varchar(1),
+cst_gndr varchar(1),
+cst_create_date date
+)
+
+create or replace table dwh.bronze.crm_prd_info(
+prd_id int,
+prd_key varchar(30),
+prd_nm varchar(30),
+prd_cost int,
+prd_line varchar(2),
+prd_start_dt date,
+prd_end_dt date)
+
+
+create or replace table dwh.bronze.crm_sales_details(
+sls_ord_num varchar(30)
+,sls_prd_key varchar(30)
+,sls_cust_id int
+,sls_order_dt int
+,sls_ship_dt int
+,sls_due_dt int
+,sls_sales int
+,sls_quantity int
+,sls_price int )
+
+
+create or replace table dwh.bronze.erp_cust_AZ12(
+CID varchar(30),
+BDATE date,
+GEN varchar(1) )
+
+
+create or replace table dwh.bronze.erp_LOC_A101(
+CID VARCHAR(30),
+CNTRY VARCHAR(30))
+
+
+create or replace table dwh.bronze.erp_X_CAT_G1V2(
+ID VARCHAR(10)
+,CAT VARCHAR(30)
+,SUBCAT VARCHAR(30)
+,MAINTENANCE VARCHAR(3))
