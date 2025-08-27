d48ffb2d4ad460d07db54c464d646b814cd45c0b
khushi1673 <63011366+khushi1673@users.noreply.github.com>
2025-08-27 18:38:34 +0530

    Update data cleaning & transformation.sql

--- scripts/SILVER/data cleaning & transformation.sql
+++ scripts/SILVER/data cleaning & transformation.sql
@@ -7,16 +7,18 @@ INSERT INTO DWH.SILVER.CRM_CUST_INFO(
 CST_ID, CST_KEY,CST_CREATE_DATE,CST_MARITAL_STATUS,CST_GNDR,CST_FIRSTNAME,CST_LASTNAME 
 )
 SELECT 
-CST_ID,CST_KEY,CST_CREATE_DATE,
+CST_ID,
+CST_KEY,
+CST_CREATE_DATE,
 CASE 
 				WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'S'
 				WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'M'
 				ELSE 'O'
 			END AS cst_marital_status, -- Normalize marital status values to readable format
 			CASE 
-				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'F'
-				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'M'
-				ELSE 'O'
+				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male' 
+				ELSE 'Unknown'
 			END AS cst_gndr,
 TRIM(CST_FIRSTNAME) AS CST_FIRSTNAME,
 TRIM(CST_LASTNAME) AS CST_LASTNAME
@@ -24,11 +26,11 @@ FROM
 (SELECT * , 
 ROW_NUMBER() OVER(PARTITION BY CST_ID ORDER BY CST_CREATE_DATE DESC)
 AS ID_RANK
-FROM CRM_CUST_INFO
+FROM bronze.CRM_CUST_INFO
 WHERE CST_ID IS NOT NULL)
 WHERE ID_RANK =1 ;
 
-
+truncate table silver.crm_cust_info;
 --------------------------------------------------------------------------------CRM_PRD_INFO-------------------------
 INSERT INTO DWH.SILVER.CRM_PRD_INFO(
 PRD_ID,PRD_NM,CATG_ID,PRD_KEY,PRD_COST,PRD_LINE,PRD_START_DT,PRD_END_DT)
@@ -43,7 +45,7 @@ PRD_START_DT,
 TRY_TO_DATE(
 LEAD(PRD_START_DT) OVER (PARTITION BY PRD_KEY ORDER BY PRD_END_DT)-1
 ) AS PRD_END_DT
-FROM CRM_PRD_INFO
+FROM bronze.CRM_PRD_INFO
 ;
 
 
@@ -60,22 +62,21 @@ sls_cust_id,
 sls_prd_key,
 sls_quantity,
 case
- when sls_order_dt = 0 or length(sls_order_dt) != 8 then null 
+ when sls_order_dt = '0' or length(sls_order_dt) != 8 then null 
  else cast(cast(sls_order_dt as varchar) as date )
 end as sls_order_dt ,
-to_date(sls_ship_dt::text, 'YYYYMMDD') as sls_ship_dt,
-to_date(sls_due_dt::text, 'YYYYMMDD') as sls_due_dt,
+cast(cast(sls_ship_dt as varchar) as date ) as sls_ship_dt,
+cast(cast(sls_due_dt as varchar) as date ) as sls_due_dt,
 case
-   when sls_sales<= 0 or sls_sales != sls_quantity * abs(sls_price)
-   then sls_quantity * abs(sls_price )
+   when sls_sales is null or sls_sales<='0' or sls_sales != sls_quantity * abs(sls_price)
+   then sls_quantity * abs(sls_price)
    else sls_sales
 end as sls_sales,
 case 
-   when sls_price <=0 or sls_price != sls_sales/ sls_quantity then sls_sales/sls_quantity
+   when  sls_price is null or sls_price <='0' then sls_sales/NULLIF(sls_quantity, 0)
    else sls_price::FLOAT
-end as sls_price,
-from crm_sales_details;
-
+end as sls_price
+from bronze.crm_sales_details;
 --------------------------------------------------------ERP_CUST_AZ12----------------------------------------
 
 INSERT INTO SILVER.ERP_CUST_AZ12(
@@ -113,4 +114,4 @@ TRIM(ID) AS ID,
 TRIM(CAT) AS CAT,
 TRIM(SUBCAT) AS SUBCAT,
 TRIM(MAINTENANCE) AS MAINTENANCE
-FROM ERP_PX_CAT_G1V2;
+FROM ERP_PX_CAT_G1V2
